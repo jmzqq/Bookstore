2dd95792ecfd2490d09b8c7d28b6dc2ddff8434d
jmzqq <joaomwguel@gmail.com>
2024-11-06 17:00:03 -0300

    feat: add create genres view

---
+++ Controllers/GenresController.cs
@@ -0,0 +1,39 @@
+﻿using Bookstore.Data;
+using Bookstore.Models;
+using Bookstore.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bookstore.Controllers
+{
+    public class GenresController : Controller
+    {
+        private readonly GenreService _service;
+
+        public GenresController(GenreService service)
+        {
+            _service = service;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_service.FindAll());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Genre genre)
+        {
+            if (!ModelState.IsValid) 
+            {
+                return View();
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
